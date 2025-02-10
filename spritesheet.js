6b7a2544c264a2f5d626ec1099907d05ec66862d
Pablogp003 <Pablogarciapampara@gmail.com>
2025-02-10 19:25:25 +0100

    Guardando cambios antes del merge

---
+++ spritesheet.js
@@ -0,0 +1,43 @@
+const INIT_SPRITESHEET = [
+{
+    key:'mario',
+    path: 'assets/entities/mario.png',
+    frameWidth: 18, 
+    frameHeight: 16
+
+},
+
+{
+    key:'goomba',
+    path: 'assets/entities/overworld/goomba.png',
+    frameWidth: 16,
+     frameHeight: 16
+
+},
+
+{
+    key:'coin',
+    path: 'assets/collectibles/coin.png',
+    frameWidth: 16,
+    frameHeight: 16
+
+},
+{
+    key: 'mario-grown',
+    path: 'assets/entities/mario-grown.png',
+    frameWidth: 18,
+    frameHeight: 32
+
+}
+
+]
+
+export const initSpritesheet = ({ load }) => {
+    INIT_SPRITESHEET.forEach(({ key, path, frameWidth,
+        frameHeight}) => {
+            load.spritesheet(key, path, {
+                frameWidth, frameHeight })
+        })
+
+
+}
